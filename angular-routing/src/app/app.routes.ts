5dfb007b3cbf35198687fca938fbc7164ab10e49
An Pham <pqan@tma.com.vn>
2024-11-20 16:44:12 +0700

    can-activate-with-auth-guard-function

--- angular-routing/src/app/app.routes.ts
+++ angular-routing/src/app/app.routes.ts
@@ -1,17 +1,18 @@
 import { Routes } from '@angular/router';
 import { FirstComponent } from './components/first/first.component';
-import { SecondComponent } from './components/second/second.component';
 import { GroceryListComponent } from './components/grocery-list/grocery-list.component';
 import { GroceryItemComponent } from './components/grocery-item/grocery-item.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { UserComponent } from './components/user/user.component';
 import { ErrorHandler, inject } from '@angular/core';
 import { ChildAComponent } from './components/child-a/child-a.component';
-import { ChildBComponent } from './components/child-b/child-b.component';
 import { HomeComponent } from './components/home/home.component';
 import { ItemsComponent } from './components/items/items.component';
 import { HeroListComponent } from './components/hero-list/hero-list.component';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { authGuard } from '../guards/auth.guard';
+import { LoginComponent } from './components/login/login.component';
 
 // title do not change when navigate to other pages without title
 // const resolvesChildATitle = () => {
@@ -19,7 +20,7 @@ import { HeroDetailComponent } from './components/hero-detail/hero-detail.compon
 // };
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/first-component', pathMatch: 'full' },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   {
     path: 'first-component',
     title: 'First Component',
@@ -28,6 +29,7 @@ export const routes: Routes = [
       {
         path: 'child-a',
         component: ChildAComponent,
+        // title: 'childA',
         title: () => Promise.resolve('Child A'),
       },
       {
@@ -48,6 +50,21 @@ export const routes: Routes = [
   },
   { path: 'home', component: HomeComponent },
   { path: 'home/items', component: ItemsComponent },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./components/login/login.component').then(
+        ({ LoginComponent }) => LoginComponent
+      ),
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./components/dashboard/dashboard.component').then(
+        ({ DashboardComponent }) => DashboardComponent
+      ),
+    canActivate: [authGuard],
+  },
   { path: 'hero', component: HeroListComponent },
   { path: 'herodetail', component: HeroDetailComponent },
   { path: 'user/:userId', component: UserComponent },
---
+++ angular-routing/src/app/components/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  login() {
+    this.authService.login();
+    this.router.navigate(['/dashboard']);
+  }
+}
